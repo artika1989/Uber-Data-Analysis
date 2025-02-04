e6bb912bac05d96ffec80e16eb1c1ea178bce57d
artika1989 <135098869+artika1989@users.noreply.github.com>
2025-02-03 21:53:09 -0800

    Update analyse_query.sql
    added sql queries for analysis

--- analyse_query.sql
+++ analyse_query.sql
@@ -11,3 +11,20 @@ JOIN `majestic-nimbus-415922.uber_data_engineering.pickup_location_dim` b
 on a.pickup_location_id = b.pickup_location_id
 group by b.pickup_location_id
 limit 10;
+
+-- Total number of trips by passenger count id
+
+select p.passenger_count_id, sum(t.trip_distance_id) as total_trip
+from `majestic-nimbus-415922.uber_data_engineering.fact_table` t
+join `majestic-nimbus-415922.uber_data_engineering.passenger_count_dim` p
+on p.passenger_count_id = t.passenger_count_id
+group by p.passenger_count_id
+order by p.passenger_count_id;
+
+-- AVERAGE FARE AMOUNT BY THE HOUR OF THE DAY
+select avg(t.fare_amount) as avg_fare_amount , p.pick_hour, p.pick_day
+from majestic-nimbus-415922.uber_data_engineering.fact_table t
+join majestic-nimbus-415922.uber_data_engineering.datetime_dim p
+on p.datetime_id = t.datetime_id
+where p.pick_day = 1
+group by p.pick_hour, p.pick_day;
